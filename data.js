487cd958ac26f2202eebdef11b94773a81928121
Angyalne Zsuzsa <holczerzs@gmail.com>
2022-03-23 14:26:11 +0100

    data.js added

---
+++ data.js
@@ -0,0 +1,42 @@
+const datas = [
+    {
+        title: "US Dollar",
+        sub: "$",
+        text: "USD"
+    },
+    {
+        title: "Euro",
+        sub: "E",
+        text: "EUR"
+    },
+    {
+        title: "Thai Baht",
+        sub: "$",
+        text: "THB"
+    },
+    {
+        title: "British Pound Sterling",
+        sub: "$",
+        text: "GBP"
+    },
+    {
+        title: "Ghanaian Cedi",
+        sub: "$",
+        text: "GHS"
+    },
+    {
+        title: "Japanese Yen",
+        sub: "$",
+        text: "JPY"
+    },
+    {
+        title: "Polish Zloty",
+        sub: "$",
+        text: "PLN"
+    },
+    {
+        title: "US Dollar",
+        sub: "$",
+        text: "USD"
+    },
+]
